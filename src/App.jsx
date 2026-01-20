0d915d427ccaa328104558fa5019f42cffc92716
pooja <https://github.com/poojatayde9960/ecom.git>
2026-01-20 10:22:25 +0530

    create aditiLandingpage UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Aditi from './pages/Aditi'
+import AditiLanding from './pages/AditiLanding'
 
 const App = () => {
   return <>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Aditi />} />
+        <Route path='/landing' element={<AditiLanding />} />
         <Route path='*' element={<h1>Page Not Found</h1>} />
       </Routes>
     </BrowserRouter>
